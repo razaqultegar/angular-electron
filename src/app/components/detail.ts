227a786e3b09c3e5b53795d2da0c9d2eb838b705
razaqultegar <myrazaqul@gmail.com>
2020-09-26 20:49:56 +0700

    Add detail components

---
+++ src/app/components/detail.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: '../templates/detail.html'
+})
+
+export class DetailComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
